c54c18b3c0a6b5f7d3db48bfaae6b0928b14ff9b
aamirali422 <aamir.ali.422aa@gmail.com>
2026-06-30 13:04:20 +0500

    Fix frontend build for deployment

--- node_modules/app/vite.config.js
+++ node_modules/app/vite.config.js
@@ -4,11 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "automatic",
-    }),
-  ],
-
-  esbuild: {
-    jsxInject: `import React from "react";`,
-  },
+      jsxRuntime: "automatic"
+    })
+  ]
 });
